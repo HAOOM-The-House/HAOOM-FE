e091c039c8a88bfacb1bee4f172930cb201ae273
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-04-18 01:55:13 +0900

    feat: Pretendard 폰트 적용

--- App.tsx
+++ App.tsx
@@ -2,10 +2,37 @@ import BottomTabNav from '@/navigators/BottomTabNav'
 import { NavigationContainer } from '@react-navigation/native'
 import { View } from 'react-native'
 import 'react-native-gesture-handler'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback } from 'react'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Black: require('./src/assets/fonts/Pretendard-Black.otf'),
+    Bold: require('./src/assets/fonts/Pretendard-Bold.otf'),
+    ExtraBold: require('./src/assets/fonts/Pretendard-ExtraBold.otf'),
+    ExtraLight: require('./src/assets/fonts/Pretendard-ExtraLight.otf'),
+    Light: require('./src/assets/fonts/Pretendard-Light.otf'),
+    Medium: require('./src/assets/fonts/Pretendard-Medium.otf'),
+    Regular: require('./src/assets/fonts/Pretendard-Regular.otf'),
+    SemiBold: require('./src/assets/fonts/Pretendard-SemiBold.otf'),
+    Thin: require('./src/assets/fonts/Pretendard-Thin.otf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded, fontError])
+
+  if (!fontsLoaded && !fontError) {
+    return null
+  }
+
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <NavigationContainer>
         <BottomTabNav />
       </NavigationContainer>
