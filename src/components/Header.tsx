400eaea010d86c8013c18d34fa611da50c513ef3
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-05-16 01:22:44 +0900

    feat: 헤더 컴포넌트 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import { colors } from '@/utils/colors'
+import styled from 'styled-components/native'
+import PrevIcon from '@/assets/images/SVGs/LeftArrow.svg'
+
+interface HeaderProps {
+  title: string
+  onPressLeftIcon: () => void
+}
+
+export default function Header({ title, onPressLeftIcon }: HeaderProps) {
+  return (
+    <Container>
+      <PrevIcon width={20} height={20} />
+      <TitleText>{title}</TitleText>
+      <EmptyIcon />
+    </Container>
+  )
+}
+
+const Container = styled.View`
+  width: 100%;
+  padding: 13px 16px;
+  flex-direction: row;
+  justify-content: space-between;
+`
+const TitleText = styled.Text`
+  font-size: 18px;
+  font-family: 'Bold';
+  color: ${colors.black};
+`
+const EmptyIcon = styled.View`
+  height: 20px;
+  width: 20px;
+`
--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,4 +1,5 @@
 export const colors = {
   main: '#FA7C8F',
   grey: '#989BA2',
+  black: '#1D1E20',
 }
