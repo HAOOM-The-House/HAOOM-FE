d6996a3d3a4f94be4c8964b4ee9f0fd95e4d2e2e
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-05-15 21:06:43 +0900

    feat: BottomTabNav 설정

--- src/navigators/BottomTabNav.tsx
+++ src/navigators/BottomTabNav.tsx
@@ -2,15 +2,75 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeNav from './HomeNav'
 import AskNav from './AskNav'
 import SearchNav from './SearchNav'
+import styled from 'styled-components/native'
+import { colors } from '@/utils/colors'
+import HomeIcon from '@/assets/images/SVGs/Home.svg'
+import MapIcon from '@/assets/images/SVGs/Map.svg'
+import InquireIcon from '@/assets/images/SVGs/Inquire.svg'
 
 const Tab = createBottomTabNavigator()
 
 export default function BottomTabNav() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="HomeNav" component={HomeNav} />
-      <Tab.Screen name="SearchNav" component={AskNav} />
-      <Tab.Screen name="AskNav" component={SearchNav} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.main,
+        tabBarStyle: {
+          height: 82,
+          borderTopColor: 'rgba(112, 115, 124, 0.16)',
+          borderTopWidth: 0.5,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="HomeNav"
+        component={HomeNav}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabWrapper>
+              <HomeIcon color={focused ? colors.main : colors.grey} width={24} height={24} />
+              <TabText style={{ color: focused ? colors.main : colors.grey }}>홈</TabText>
+            </TabWrapper>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="SearchNav"
+        component={AskNav}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabWrapper>
+              <MapIcon color={focused ? colors.main : colors.grey} width={24} height={24} />
+              <TabText style={{ color: focused ? colors.main : colors.grey }}>지점찾기</TabText>
+            </TabWrapper>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AskNav"
+        component={SearchNav}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabWrapper>
+              <InquireIcon color={focused ? colors.main : colors.grey} width={24} height={24} />
+              <TabText style={{ color: focused ? colors.main : colors.grey }}>가맹문의</TabText>
+            </TabWrapper>
+          ),
+        }}
+      />
     </Tab.Navigator>
   )
 }
+
+const TabWrapper = styled.View`
+  display: flex;
+  gap: 6px;
+  align-items: center;
+  padding-top: 10px;
+`
+const TabText = styled.Text`
+  font-family: 'Medium';
+  font-size: 12px;
+`
---
+++ src/utils/colors.ts
@@ -0,0 +1,4 @@
+export const colors = {
+  main: '#FA7C8F',
+  grey: '#989BA2',
+}
