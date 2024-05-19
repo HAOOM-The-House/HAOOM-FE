801b1f1bde8356725e3d6e5bb12600812e55e1c0
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-05-20 02:58:59 +0900

    fix: Reverse Geocoding api 실행 조건 변경

--- src/hooks/queries/ReverseGeocode.ts
+++ src/hooks/queries/ReverseGeocode.ts
@@ -7,8 +7,9 @@ function useGetAddress() {
   const [longitude] = useAtom(pinLongitudeAtom)
   const [latitude] = useAtom(pinLatitudeAtom)
   const { data } = useQuery({
-    queryKey: ['getAddress'],
+    queryKey: ['getAddress', latitude, longitude],
     queryFn: () => getAddress({ longitude, latitude }),
+    enabled: longitude !== 0 && latitude !== 0,
   })
   return { data }
 }
