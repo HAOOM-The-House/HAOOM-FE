b0649692900deab59a2e995bf0fd7144bc729ca2
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-05-22 05:12:44 +0900

    fix: 안드로이드 하단바에 의해 앱 잘려보이는 현상 수정

--- src/components/ScreenLayout.tsx
+++ src/components/ScreenLayout.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components/native'
 import React from 'react'
 import { Platform } from 'react-native'
 import { StatusBar } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 interface ScreenLayoutProps {
   children: React.ReactNode
@@ -9,9 +10,14 @@ interface ScreenLayoutProps {
 }
 
 export default function ScreenLayout({ children, background = 'white' }: ScreenLayoutProps) {
+  const insets = useSafeAreaInsets()
   return (
     <ScreenContainer
-      style={{ backgroundColor: background, paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }}
+      style={{
+        backgroundColor: background,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+        paddingBottom: Platform.OS === 'android' ? insets.bottom : 0,
+      }}
     >
       {children}
     </ScreenContainer>
