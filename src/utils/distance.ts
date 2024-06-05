46d79f259de48961b0cf73f87ec404fd8dd25f31
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-06-06 02:34:44 +0900

    fix: 미터 단위 거리일 때도 소수점 첫째자리까지만 보여주게 수정

--- src/utils/distance.ts
+++ src/utils/distance.ts
@@ -3,7 +3,10 @@ export const getDistance = (distance: number) => {
     const kilometer = distance / 1000
     if (Number.isInteger(kilometer)) return String(kilometer) + 'KM'
     else return String(kilometer.toFixed(1)) + 'KM'
+    if (Number.isInteger(kilometer)) return kilometer + 'KM'
+    else return kilometer.toFixed(1) + 'KM'
   } else {
     return String(distance) + 'M'
+    return distance.toFixed(1) + 'M'
   }
 }
